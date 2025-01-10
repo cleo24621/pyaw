c69ccbb4f0142fee2410d00d403f43a53aab5c23
cleo <cleo24620@gmail.com>
2025-01-11 00:09:51 +0800

    20250111, acer_laptop update.

---
+++ test.py
@@ -0,0 +1,6 @@
+from pyaw import swarm
+
+
+fp_e = r"\\Diskstation1\file_three\aw\swarm\A\vfm50\sw_vfm50A_20160311T060000_20160311T070000_0.pkl"
+df_e = swarm.Swarm(fp_e,'vfm50','20160311T060000','20160311T070000')
+
